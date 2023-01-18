94a49e3f34aea748f428c35481548ca6e048e522
rhammock1 <robert.hammock@gotab.io>
2023-01-18 10:39:49 -0500

    WIP crypto trading

---
+++ crypto/index.js
@@ -0,0 +1,9 @@
+// Keeping crypto seperate even though it may employ the example algorithms
+
+const run = async () => {
+  console.log('Crypto example running');
+};
+
+const name = () => 'Crypto';
+
+module.exports = {run, name};
