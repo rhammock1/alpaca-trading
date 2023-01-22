e5e2f2a6affe6044397ca564a3e2b4fd5d549fbc
rhammock1 <robert.hammock@gotab.io>
2023-01-21 19:33:38 -0500

    switch to one class, multiple stocks

--- examples/mean-revision-example.js
+++ examples/mean-revision-example.js
@@ -46,7 +46,7 @@ class MeanRevision {
     log('info', 'Market opened.');
     
     log('info', 'Getting running average.');
-    await this.getRunningAverage();
+    await Promise.all(this.stocks.map(stock => this.getRunningAverage(stock)));
 
     await spin(this.run.bind(this), this.rebalance.bind(this));
   }
@@ -54,26 +54,26 @@ class MeanRevision {
   /**
    * @description Get the running average of the selected stock
    */
-  async getRunningAverage() {
+  async getRunningAverage(stock) {
     const bar_checker = setInterval(async () => {
       try {
         const response = await this.alpaca.getCalendar(Date.now());
         const market_open = response[0].open;
         const resp = await this.alpaca.getBars('minute', this.stock, {start: market_open, limit: AVERAGE_DIVISOR});
-        const bars = resp[this.stock];
+        const bars = resp[stock];
         if(bars.length >= AVERAGE_DIVISOR) {
-          log('debug', {stock: this.stock}, 'Got bars.');
-          this.runningAverage = 0;
-          this.current_price = bars[bars.length - 1].closePrice;
+          log('debug', {stock}, 'Got bars.');
+          this.runningAverage[stock] = 0;
+          this.current_price[stock] = bars[bars.length - 1].closePrice;
           for(const bar of bars) {
-            this.runningAverage += bar.closePrice;
+            this.runningAverage[stock] += bar.closePrice;
           }
           clearInterval(bar_checker);
-          this.runningAverage /= AVERAGE_DIVISOR;
-          log('debug', {stock: this.stock}, `Running average is ${this.runningAverage}`);
+          this.runningAverage[stock] /= AVERAGE_DIVISOR;
+          log('debug', {stock}, `Running average for ${stock} is ${this.runningAverage[stock]}`);
         }
       } catch(err) {
-        log('error', 'Error while getting bars for running average.', err);
+        log('error', {stock}, 'Error while getting bars for running average.', err);
       }
     }, 60000);
   }
@@ -82,112 +82,60 @@ class MeanRevision {
    * @description Rebalance our position after an update
    */
   async rebalance() {
-    let position_qty = 0;
-    let position_value = 0;
+    let positions;
 
     // Get our position, if any
     try {
-      const response = await this.alpaca.getPosition(this.stock);
-      position_qty = response.qty;
-      position_value = response.market_value;
+      positions = await this.alpaca.getPositions();
     } catch(err) {
       log('error', 'Error while getting position in rebalance.', err, err.response, err.config, err.toJSON());
     }
 
     // Get the new updated prices and running average
-    await this.getRunningAverage();
+    await Promise.all(this.stocks.map(stock => this.getRunningAverage(stock)));
 
-    if(this.current_price > this.runningAverage) {
-      // Sell our position if the price is above the running average
-      if(position_qty > 0) {
-        log('info', {stock: this.stock}, 'Price is above running average. Selling position.');
-        await this.submitLimitOrder(position_qty, 'sell');
-      } else {
-        log('info', {stock: this.stock}, 'No position in the stock. No action taken.');
-      }
-    } else if(this.current_price < this.running_average) {
-      // Determine optimal amount of shares based on portfolio and market data
-      let portfolio_value = 0;
-      let buying_power = 0;
-      try {
-        const account = await this.alpaca.getAccount();
-        ({portfolio_value, buying_power} = account);
-      } catch(err) {
-        log('error', 'Error while getting account details in rebalance', err);
-      }
-      const portfolio_share = ((this.running_average - this.current_price) / this.current_price) * 200;
-      const target_position_value = portfolio_value * portfolio_share;
-      let amount_to_add = target_position_value - position_value;
-
-      // Add to our position, constrained by our buying power. Or, sell down to optimal amount of shares
-      const expression = add => Math.floor(add / this.current_price);
-      if(amount_to_add > 0) {
-        if(amount_to_add > buying_power) {
-          amount_to_add = buying_power;
+    await Promise.all(positions.map(async (position) => {
+      const {symbol: stock, qty, market_value} = position;
+      if(this.current_price[stock] > this.runningAverage[stock]) {
+        // Sell our position if the price is above the running average
+        if(qty > 0) {
+          log('info', {stock}, 'Price is above running average. Selling position.');
+          await submitOrder({quantity: qty, stock, type: 'limit', side: 'sell'});
+        } else {
+          log('info', {stock}, 'No position in the stock. No action taken.');
         }
-        const quantity_to_buy = expression(amount_to_add);
-        await this.submitLimitOrder(quantity_to_buy, 'buy');
-      } else {
-        amount_to_add *= -1;
-        const quantity_to_sell = expression(amount_to_add) > position_qty
-          ? position_qty
-          : expression(amount_to_add);
-        await this.submitLimitOrder(quantity_to_sell, 'sell');
-      }
-    }
-  }
+      } else if(this.current_price[stock] < this.running_average[stock]) {
+        // Determine optimal amount of shares based on portfolio and market data
+        let portfolio_value = 0;
+        let buying_power = 0;
+        try {
+          const account = await this.alpaca.getAccount();
+          ({portfolio_value, buying_power} = account);
+        } catch(err) {
+          log('error', 'Error while getting account details in rebalance', err);
+        }
+        const portfolio_share = (
+          (this.running_average[stock] - this.current_price[stock]) / this.current_price[stock]) * 200;
+        const target_position_value = portfolio_value * portfolio_share;
+        let amount_to_add = target_position_value - market_value;
 
-  /**
-   * @description Submit a limit order if quantity is above 0.
-   * @param {number} qty - quantity of shares to buy or sell
-   * @param {string} side - buy or sell
-   */
-  async submitLimitOrder(qty, side) {
-    const log_details = {stock: this.stock, quantity: qty, side};
-    if(qty > 0) {
-      try {
-        const order = await this.alpaca.createOrder({
-          symbol: this.stock,
-          qty,
-          side,
-          type: 'limit',
-          time_in_force: 'day',
-          limit_price: this.current_price,
-        });
-        log('info', log_details, 'Limit order completed.');
-        this.last_order = order;
-      } catch(err) {
-        log('error', log_details, 'Error while submitting limit order.', err);
+        // Add to our position, constrained by our buying power. Or, sell down to optimal amount of shares
+        const expression = add => Math.floor(add / this.current_price[stock]);
+        if(amount_to_add > 0) {
+          if(amount_to_add > buying_power) {
+            amount_to_add = buying_power;
+          }
+          const quantity_to_buy = expression(amount_to_add);
+          await submitOrder({qty: quantity_to_buy, stock, type: 'limit', side: 'buy'});
+        } else {
+          amount_to_add *= -1;
+          const quantity_to_sell = expression(amount_to_add) > qty
+            ? qty
+            : expression(amount_to_add);
+          await submitOrder({quantity: quantity_to_sell, stock, type: 'limit', side: 'sell'});
+        }
       }
-    } else {
-      log('warn', log_details, 'Quantity is less than or equal to 0. Not submitting order.');
-    }
-  }
-
-  /**
-   * @description Submit a market order if quantity is above 0.
-   * @param {number} qty - quantity of shares to buy or sell
-   * @param {string} side - buy or sell
-   */
-  async submitMarketOrder(qty, side) {
-    const log_details = {stock: this.stock, quantity: qty, side};
-    if(qty > 0) {
-      try {
-        const order = await this.alpaca.createOrder({
-          symbol: this.stock,
-          qty,
-          side,
-          type: 'market',
-          time_in_force: 'day',
-        });
-        log('info', log_details, 'Market order completed.');
-        this.last_order = order;
-      } catch(err) {
-        log('error', log_details, 'Error while submitting market order.', err);
-      }
-    } else {
-      log('warn', log_details, 'Quantity is less than or equal to 0. Not submitting order.');
-    }
+    }));
   }
 }
 
